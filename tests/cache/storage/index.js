da169ff99459ffe51940af0b7e73b135c0f8e52e
nemanjan00 <nemanjan00@gmail.com>
2020-01-10 20:44:43 +0100

    Store and get test

--- tests/cache/storage/index.js
+++ tests/cache/storage/index.js
@@ -17,11 +17,46 @@ module.exports = (name, engine) => {
 		});
 
 		it("Stores kev-value pairs", function(done) {
-			done();
+			engine.then(implementation => {
+				if(implementation.store === undefined) {
+					done("There is no store function");
+					return;
+				}
+
+				implementation.store("name", "value").then(() => {
+					done();
+				}).catch((error) => {
+					done(error);
+				});
+			});
 		});
 
 		it("Returns stored kev-value pairs", function(done) {
-			done();
+			engine.then(implementation => {
+				if(implementation.store === undefined) {
+					done("There is no store function");
+					return;
+				}
+
+				implementation.store("name", "value").then(() => {
+					if(implementation.get === undefined) {
+						done("There is no get function");
+						return;
+					}
+
+					implementation.get("name").then((value) => {
+						if(value == "value") {
+							done();
+						} else {
+							done("Wrong value returned");
+						}
+					}).catch((error) => {
+						done(error);
+					});
+				}).catch((error) => {
+					done(error);
+				});
+			});
 		});
 
 		it("Stored key-value pairs expire", function(done) {
