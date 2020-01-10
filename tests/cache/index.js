80a693dec6252c499a5b8e42756288f1cd678c77
nemanjan00 <nemanjan00@gmail.com>
2020-01-10 20:38:56 +0100

    Fail for undefined protocols

--- tests/cache/index.js
+++ tests/cache/index.js
@@ -1,6 +1,14 @@
 const cache = require("../../src/cache");
 
 describe("cache", function() {
+	it("It fails, when wront protocol for cache is configured", function(done) {
+		cache("justsomebullchitprotocol://").then(() => {
+			done("Accepted invalid protocol");
+		}).catch(() => {
+			done();
+		});
+	});
+
 	require("./storage")("Memory", cache("memory://"));
 	require("./storage")("Redis", cache("redis://127.0.0.1:6379"));
 });
